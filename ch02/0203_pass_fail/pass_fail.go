8c8160a885bd2c269223caacb9e7803ab526aee3
Zetta Kim <zetta@iMac.local>
2020-04-11 23:04:42 +0900

    first commit

---
+++ ch02/0203_pass_fail/pass_fail.go
@@ -0,0 +1,35 @@
+// pass_fail 프로그램은 성적의 합격 여부를 알려 줍니다.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+
+	fmt.Println("A Grade of", grade, "is", status)
+}
