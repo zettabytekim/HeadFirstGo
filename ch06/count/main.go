ca33f18946c428c4a722d1152975156c8b5f609f
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-03 22:46:19 +0900

    5/3 commit

---
+++ ch06/count/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"HeadFirstGo/ch05/datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var names []string
+	var counts []int
+
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+		}
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+
+}
