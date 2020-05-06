5b0523277fe128524e1b6853461544925271bbcc
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-06 22:49:29 +0900

    5/6 commit

--- ch07/map/main.go
+++ ch07/map/main.go
@@ -40,4 +40,53 @@ func main() {
 	fmt.Println("Necklace:", jewelry["necklace"])
 	fmt.Println("Shirts:", clothing["shirts"])
 	fmt.Println("Pants:", clothing["pants"])
+
+	counters := map[string]int{"a": 3, "b": 0}
+	var value int
+	var ok bool
+	value, ok = counters["a"]
+	fmt.Println(value, ok)
+	value, ok = counters["b"]
+	fmt.Println(value, ok)
+	value, ok = counters["c"]
+	fmt.Println(value, ok)
+	fmt.Println()
+
+	_, ok = counters["b"]
+	fmt.Println(ok)
+	_, ok = counters["c"]
+	fmt.Println(ok)
+	fmt.Println()
+
+	status("Alma")
+	status("Carl")
+	fmt.Println()
+	wordCount()
+}
+
+func status(name string) {
+	grades := map[string]float64{"Alma": 0, "Roit": 86.5}
+	grade, ok := grades[name]
+	if !ok {
+		fmt.Printf("No grade recorded for %s.\n", name)
+	} else if grade < 60 {
+		fmt.Printf("%s is failing!\n", name)
+	}
+}
+
+func wordCount() {
+	data := []string{"a", "c", "e", "a", "e"}
+	counts := make(map[string]int)
+	for _, item := range data {
+		counts[item]++
+	}
+	letters := []string{"a", "b", "c", "d", "e"}
+	for _, letter := range letters {
+		count, ok := counts[letter]
+		if !ok {
+			fmt.Printf("%s: not found\n", letter)
+		} else {
+			fmt.Printf("%s: %d\n", letter, count)
+		}
+	}
 }
