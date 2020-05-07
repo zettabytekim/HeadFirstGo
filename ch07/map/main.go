20998f7395437faf442c9da7366995fcc8c6d56c
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-07 22:50:59 +0900

    5/7 commit

--- ch07/map/main.go
+++ ch07/map/main.go
@@ -62,6 +62,9 @@ func main() {
 	status("Carl")
 	fmt.Println()
 	wordCount()
+
+	fmt.Println()
+	mapDelete()
 }
 
 func status(name string) {
@@ -89,4 +92,27 @@ func wordCount() {
 			fmt.Printf("%s: %d\n", letter, count)
 		}
 	}
+}
+
+func mapDelete() {
+	var ok bool
+	ranks := make(map[string]int)
+	var rank int
+	ranks["bronze"] = 3
+	rank, ok = ranks["bronze"]
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+
+	delete(ranks, "bronze")
+	rank, ok = ranks["bronze"]
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+	fmt.Println()
+
+	isPrime := make(map[int]bool)
+	var prime bool
+	isPrime[5] = true
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
+	delete(isPrime, 5)
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
 }
