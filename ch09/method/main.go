a1c92fbf4ff9902269a42369466cdf9916405db4
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-13 22:14:26 +0900

    5/13 final commit

--- ch09/method/main.go
+++ ch09/method/main.go
@@ -4,8 +4,21 @@ import "fmt"
 
 type MyType string
 
+type Number int
+
+func (n *Number) Double() {
+	*n *= 2
+}
+
 func (m MyType) sayHi() {
-	fmt.Println("Hi")
+	fmt.Println("Hi from", m)
+}
+
+func (m MyType) method() {
+	fmt.Println("Method with value receiver")
+}
+func (m *MyType) pointerMethod() {
+	fmt.Println("Method with pointer receiver")
 }
 
 func main() {
@@ -13,4 +26,17 @@ func main() {
 	value.sayHi()
 	anotherValue := MyType("another value")
 	anotherValue.sayHi()
+	fmt.Println()
+
+	number := Number(4)
+	fmt.Println("Original:", number)
+	number.Double()
+	fmt.Println("after:", number)
+
+	value2 := MyType("a value")
+	pointer2 := &value2
+	value2.method()
+	value2.pointerMethod()
+	pointer2.method()
+	pointer2.pointerMethod()
 }
