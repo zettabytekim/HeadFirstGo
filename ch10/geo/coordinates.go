2e449c7f9a05abdb3147c34a570d3d5fdc4f53f7
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-16 22:37:54 +0900

    ch10 method final commit

--- ch10/geo/coordinates.go
+++ ch10/geo/coordinates.go
@@ -1,17 +1,37 @@
 package geo
 
+import "errors"
+
 // Coordinates struct...
 type Coordinates struct {
-	Latutude  float64
-	Longitude float64
+	latitude  float64
+	longitude float64
+}
+
+// Latitude ...
+func (c *Coordinates) Latitude() float64 {
+	return c.latitude
+}
+
+// Logitude ...
+func (c *Coordinates) Longitude() float64 {
+	return c.longitude
 }
 
 // SetLatitude ...
-func (c *Coordinates) SetLatitude(latitude float64) {
-	c.Latutude = latitude
+func (c *Coordinates) SetLatitude(latitude float64) error {
+	if latitude < -90 || latitude > 90 {
+		return errors.New("invalid latitude")
+	}
+	c.latitude = latitude
+	return nil
 }
 
 // SetLongitude ...
-func (c *Coordinates) SetLongitude(longitude float64) {
-	c.Longitude = longitude
+func (c *Coordinates) SetLongitude(longitude float64) error {
+	if longitude < -180 || longitude > 180 {
+		return errors.New("invalid longitude")
+	}
+	c.longitude = longitude
+	return nil
 }
