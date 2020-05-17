369700956c4c2b17e7342f7194e4c04bea063ed9
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-17 09:23:30 +0900

    ch11 interface 1st commit

---
+++ ch11/player.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/zettakim/gadget"
+
+func playList(device gadget.TapePlayer, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func main() {
+	player := gadget.TapePlayer{}
+	mixtape := []string{"Jessie's Girl", "Whip It", "9 to 5"}
+	playList(player, mixtape)
+}
