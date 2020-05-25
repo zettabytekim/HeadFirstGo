da47025c5f715cc43006dbf23353d2495aac7857
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-25 21:20:03 +0900

    5/25 commit

---
+++ ch11/interface3.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type Car string
+
+func (c Car) Accelerate() {
+	fmt.Println("Speeding up")
+}
+func (c Car) Brake() {
+	fmt.Println("Stopping")
+}
+func (c Car) Steer(direction string) {
+	fmt.Println("Turning", direction)
+}
+
+type Truck string
+
+func (t Truck) Accelerate() {
+	fmt.Println("Speeding up")
+}
+func (t Truck) Brake() {
+	fmt.Println("Stopping")
+}
+func (t Truck) Steer(direction string) {
+	fmt.Println("Turning", direction)
+}
+func (t Truck) LoadCargo(cargo string) {
+	fmt.Println("Loading", cargo)
+}
+
+type Vehicle interface {
+	Accelerate()
+	Brake()
+	Steer(string)
+}
+
+func main() {
+	var vehicle Vehicle = Car("Toyota Yavic")
+	vehicle.Accelerate()
+	vehicle.Steer("left")
+
+	vehicle = Car("Ford F180")
+	vehicle.Brake()
+	vehicle.Steer("right")
+}
--- ch11/player.go
+++ ch11/player.go
@@ -2,7 +2,12 @@ package main
 
 import "github.com/zettakim/gadget"
 
-func playList(device gadget.TapePlayer, songs []string) {
+type Player interface {
+	Play(string)
+	Stop()
+}
+
+func playList(device Player, songs []string) {
 	for _, song := range songs {
 		device.Play(song)
 	}
@@ -10,7 +15,9 @@ func playList(device gadget.TapePlayer, songs []string) {
 }
 
 func main() {
-	player := gadget.TapePlayer{}
 	mixtape := []string{"Jessie's Girl", "Whip It", "9 to 5"}
+	var player Player = gadget.TapePlayer{}
+	playList(player, mixtape)
+	player = gadget.TapeRecorder{}
 	playList(player, mixtape)
 }
