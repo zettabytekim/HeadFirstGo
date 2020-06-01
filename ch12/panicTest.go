ea7abda59380ed429293b984636f5aac4bc847c3
zettabyte.kim@gmail.com <Zetta Kim>
2020-06-01 18:23:29 +0900

    6/1 커밋

---
+++ ch12/panicTest.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	one()
+}
+
+func one() {
+	defer fmt.Println("deferred in one()")
+	two()
+}
+func two() {
+	defer fmt.Println("deferred in two()")
+	panic("Let's see whay's been deffered!")
+}
--- ch12/scanDirectory.go
+++ ch12/scanDirectory.go
@@ -10,13 +10,18 @@ func scanDirectory(path string) error {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return nil
+		panic(err)
 	}
-
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() {
+			scanDirectory(filePath)
+		} else {
+			fmt.Println(filePath)
+		}
 	}
+	return nil
 }
 func main() {
-
+	scanDirectory("go")
 }
