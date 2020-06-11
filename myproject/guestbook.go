8a8613b43390748c50cde2c89ed7eff099cf7943
Zetta Kim <zettabyte.kim@gmail.com>
2020-06-11 21:30:41 +0900

    6/11 commit

--- myproject/guestbook.go
+++ myproject/guestbook.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
 
 func check(err error) {
 	if err != nil {
@@ -12,16 +36,38 @@ func check(err error) {
 	}
 }
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	// plcaceholder := []byte("signature list goes here")
-	// _, err := writer.Write(plcaceholder)
+	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
 	check(err)
 	err = html.Execute(writer, nil)
 	check(err)
 }
 
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	option := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", option, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
